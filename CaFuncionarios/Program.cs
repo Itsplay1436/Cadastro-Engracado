9f3a7a9c38e7703e28902a1ae947b08c041fb9b5
Ítalo Campitelli <itsplay1436.italoc@gmail.com>
2026-03-13 23:29:35 -0300

    Add files via upload

---
+++ CaFuncionarios/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CaFuncionarios;
+
+Console.Title = "Cadastro de Funcionários";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Black;
+
+try
+{
+    do
+    {
+        Console.Clear();
+
+        Funcionarios funcionario;
+
+        Console.WriteLine(Console.Title);
+        Console.WriteLine("Escolha uma das opções abaixo:" +
+            "\n1. Cadastrar funcionário" +
+            "\n2. Listar funcionário" +
+            "\n3. Editar funcionário" +
+            "\n4. Consultar funcionário" +
+            "\n5. Excluir funcionário" +
+            "\n0. Sair");
+        //Continuaremos aqui
+    }
+    while (true);
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("Aconteceu um erro: \n" + ex.Message);
+}
