d7488ae1eb4d2e22e415dc376f0eff3184ae4ada
Ítalo Campitelli <itsplay1436.italoc@gmail.com>
2026-03-27 11:40:35 -0300

    la eleeee XD

---
+++ SoFuncionarios/CaFuncionarios/Funcionarios.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaFuncionarios
+{
+    internal class Funcionarios
+    {
+        private int matricula;
+
+        public int Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        private string nome;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private string endereco;
+
+        public string Endereco
+        {
+            get { return endereco; }
+            set { endereco = value; }
+        }
+
+        private string cidade;
+
+        public string Cidade
+        {
+            get { return cidade; }
+            set { cidade = value; }
+        }
+
+        //unidade federativa
+        private string uf;
+
+        public string Uf
+        {
+            get { return uf; }
+            set { uf = value; }
+        }
+
+        private string cep;
+
+        public string Cep
+        {
+            get { return cep; }
+            set { cep = value; }
+        }
+
+        private string cpf;
+
+        public string Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+
+        private string telefone;
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private DateTime datanasc;
+
+        public DateTime DataNasc
+        {
+            get { return datanasc; }
+            set { datanasc = value; }
+        }
+
+        public Funcionarios()
+        {
+            Matricula = 0;
+            Nome = "";
+            Endereco = "";
+            Cidade = "";
+            Uf = "";
+            Cep = "";
+            Cpf = "";
+            Telefone = "";
+            Email = "";
+            DataNasc = DateTime.Now;
+        }
+
+        public Funcionarios(int matricula, string nome,
+            string endereco, string cidade, string uf,
+            string cep, string cpf, string telefone,
+            string email, DateTime dataNasc)
+        {
+            Matricula = matricula;
+            Nome = nome;
+            Endereco = endereco;
+            Cidade = cidade;
+            Uf = uf;
+            Cep = cep;
+            Cpf = cpf;
+            Telefone = telefone;
+            Email = email;
+            DataNasc = dataNasc;
+        }
+        public int Idade()
+        {
+            int idade = DateTime.Now.Year - DataNasc.Year;
+            if (DateTime.Now < DataNasc.AddYears(idade))
+                idade--;
+            return idade;
+        }
+
+        public override string ToString()
+        {
+            return "Funcionário:" +
+                $"\nMatrícula: {Matricula}" +
+                "\nNome: " + Nome +
+                "\nEndereço: " + Endereco +
+                "\nCidade: " + Cidade +
+                "\nUF: " + Uf +
+                "\nCEp: " + Cep +
+                "\nCPF: " + Cpf +
+                "\nTelefone: " + Telefone +
+                "\nEmail: " + Email +
+                //"\nData de Nascimento: " +
+                "\nIdade: " + Idade();
+        }
+    }
+}
---
+++ SoFuncionarios/CaFuncionarios/Program.cs
@@ -0,0 +1,223 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CaFuncionarios;
+using System;
+using System.Linq;
+
+Console.Title = "Cadastro de Funcionários";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Black;
+
+List<Funcionarios> funcionarios = new List<Funcionarios>();
+
+try
+{
+    int opcao = 0;
+    do
+    {
+        Console.Clear();
+
+        Funcionarios funcionario;
+
+        Console.WriteLine(Console.Title);
+        Console.WriteLine("Escolha uma das opções abaixo:" +
+            "\n1. Cadastrar funcionário" +
+            "\n2. Listar funcionários" +
+            "\n3. Editar funcionário" +
+            "\n4. Consultar funcionário" +
+            "\n5. Excluir funcionário" +
+            "\n0. Sair");
+
+        opcao = int.Parse(Console.ReadLine()!);
+
+        switch (opcao)
+        {
+            case 1://cadastrar funcionario
+                Console.Clear();
+                funcionario = new Funcionarios();
+
+                Console.WriteLine("Cadastrar um Funcionario");
+                Console.WriteLine("Informe a matricula");
+                int novaMatricula = int.Parse(Console.ReadLine()!);
+
+                if (funcionarios.Any(f => f.Matricula == novaMatricula))//verificar matricula já existente
+                {
+                    Console.WriteLine("Já existe um funcionário com essa matrícula!");
+                    break;
+                }
+
+                funcionario.Matricula = novaMatricula;
+
+                Console.WriteLine("Informe o nome:");
+                funcionario.Nome = Console.ReadLine()!;
+                Console.WriteLine("Informe o endereço:");
+                funcionario.Endereco = Console.ReadLine()!;
+                Console.WriteLine("Informe a cidade:");
+                funcionario.Cidade = Console.ReadLine()!;
+                Console.WriteLine("Informe o estado:");
+                funcionario.Uf = Console.ReadLine()!;
+                Console.WriteLine("Informe o CEP:");
+                funcionario.Cep = Console.ReadLine()!;
+                Console.WriteLine("Informe o CPF:");
+                funcionario.Cpf = Console.ReadLine()!;
+                Console.WriteLine("Informe o telefone:");
+                funcionario.Telefone = Console.ReadLine()!;
+                Console.WriteLine("Informe o email:");
+                funcionario.Email = Console.ReadLine()!;
+                Console.WriteLine("Informe a Data de Nascimento (dd/mm/aaaa):");
+                funcionario.DataNasc = DateTime.Parse(Console.ReadLine()!);
+
+                funcionarios.Add(funcionario);
+                Console.WriteLine("Funcionário cadastrado com sucesso!");
+                break;
+
+            case 2://listar funcionario
+                Console.Clear();
+                Console.WriteLine("Listagem dos Funcionarios");
+
+                if (funcionarios.Count == 0)
+                {
+                    Console.WriteLine("Nenhum funcionário cadastrado.");
+                }
+                else
+                {
+                    foreach (var item in funcionarios)
+                    {
+                        Console.WriteLine(item.ToString() + "\n");
+                    }
+                }
+                break;
+
+            case 3://editar funcionario
+                Console.Clear();
+                Console.WriteLine("Alterando Funcionario");
+                Console.WriteLine("Informe a matricula desejada:");
+                int mat = int.Parse(Console.ReadLine()!);
+
+                bool naoAchou = true;
+
+                foreach (var func in funcionarios)
+                {
+                    if (func.Matricula == mat)
+                    {
+                        naoAchou = false;
+
+                        Console.WriteLine("Informe o nome:");
+                        func.Nome = Console.ReadLine()!;
+                        Console.WriteLine("Informe o endereço:");
+                        func.Endereco = Console.ReadLine()!;
+                        Console.WriteLine("Informe a cidade:");
+                        func.Cidade = Console.ReadLine()!;
+                        Console.WriteLine("Informe o estado:");
+                        func.Uf = Console.ReadLine()!;
+                        Console.WriteLine("Informe o CEP:");
+                        func.Cep = Console.ReadLine()!;
+                        Console.WriteLine("Informe o CPF:");
+                        func.Cpf = Console.ReadLine()!;
+                        Console.WriteLine("Informe o telefone:");
+                        func.Telefone = Console.ReadLine()!;
+                        Console.WriteLine("Informe o email:");
+                        func.Email = Console.ReadLine()!;
+                        Console.WriteLine("Informe a Data de Nascimento:");
+                        func.DataNasc = DateTime.Parse(Console.ReadLine()!);
+
+                        Console.WriteLine("Alterado com sucesso!");
+                        break;
+                    }
+                }
+
+                if (naoAchou)
+                {
+                    Console.WriteLine("Usuário não encontrado");
+                }
+                break;
+
+            case 4://consulta
+                Console.Clear();
+                Console.WriteLine("Consulta do Funcionario");
+                Console.WriteLine("Informe a matricula que deseja consultar: ");
+                int con = int.Parse(Console.ReadLine()!);
+
+                bool encontrou = false;
+
+                foreach (var item in funcionarios)
+                {
+                    if (item.Matricula == con)
+                    {
+                        Console.WriteLine(item.ToString() + "\n");
+                        encontrou = true;
+                    }
+                }
+
+                if (!encontrou)
+                {
+                    Console.WriteLine("Funcionário não encontrado.");
+                }
+                break;
+
+            case 5://excluir
+                Console.Clear();
+                Console.WriteLine("Excluir Funcionário");
+                Console.WriteLine("Informe a matrícula que deseja excluir: ");
+                int matriculaExcluir = int.Parse(Console.ReadLine()!);
+
+                Funcionarios? funcionarioRemover = null;
+
+                foreach (var func in funcionarios)
+                {
+                    if (func.Matricula == matriculaExcluir)
+                    {
+                        funcionarioRemover = func;
+                        break;
+                    }
+                }
+
+                if (funcionarioRemover != null)//confirmar exclusão
+                {
+                    Console.Clear();
+                    Console.WriteLine("Confira os dados do funcionário:\n");
+
+                    Console.WriteLine($"Matrícula: {funcionarioRemover.Matricula}");
+                    Console.WriteLine($"Nome: {funcionarioRemover.Nome}");
+                    Console.WriteLine($"CPF: {funcionarioRemover.Cpf}");
+                    Console.WriteLine($"Email: {funcionarioRemover.Email}");
+                    Console.WriteLine($"Telefone: {funcionarioRemover.Telefone}");
+
+                    Console.WriteLine("\nDigite novamente a matrícula para confirmar a exclusão:");
+                    int confirmacaoMatricula = int.Parse(Console.ReadLine()!);
+
+                    if (confirmacaoMatricula == funcionarioRemover.Matricula)
+                    {
+                        funcionarios.Remove(funcionarioRemover);
+                        Console.WriteLine("Funcionário removido com sucesso!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Matrícula não confere. Exclusão cancelada.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Funcionário não encontrado.");
+                }
+                break;
+
+            case 0:
+                Console.WriteLine("Até Breve!");
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida!");
+                break;
+        }
+
+        Console.WriteLine("Pressione uma tecla para continuar...");
+        Console.ReadKey();
+        Console.Clear();
+
+    } while (opcao != 0);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Aconteceu um erro: \n" + ex.Message);
+}
